9a622381248b0844790564918a16d705af99f7c1
Gustavo <gustavo.prog40@gmail.com>
2023-01-19 14:16:53 -0300

    yup fields done

--- src/Controllers/transationController.ts
+++ src/Controllers/transationController.ts
@@ -36,55 +36,50 @@ async function register(req: Request, res: Response) {
   try {
     const schema = Yup.object({
       cartCode: Yup.string().required(),
-      installments: Yup.number().min(1).when("paymentType",
-        (paymentType, schema) => paymentType === "credit_card" ? schema.max(12) :
-         schema.max(1)
+      installments: Yup.number()
+        .min(1)
+        .when('paymentType', (paymentType, schema) =>
+          paymentType === 'credit_card' ? schema.max(12) : schema.max(1)
         ),
 
-        paymentType: Yup.mixed().oneOf(["billet", "credit_card"]).required(),
+      paymentType: Yup.mixed().oneOf(['billet', 'credit_card']).required(),
       total: Yup.string().required(),
       customerName: Yup.string().min(3),
       customerEmail: Yup.string().required().email(),
-      // customerMobile: Yup.string().required()
-      //   .test("is-valid-mobile",
-      //   "${path} is not a valide mobile number",
-      //     (value: any) => parsePhoneNumber(value, "BR").isValid()),
-      //  customerDocument: Yup.string().required()
-      //   .test("is-valid-document",
+      customerMobile: Yup.string()
+        .required()
+        .test('is-valid-mobile', '${path} is not a valide mobile number', (value: any) =>
+          parsePhoneNumber(value, 'BR').isValid()
+        ),
+      //  customerDocument: Yup.string().required().test("is-valid-document",
       //   "${path} is not a valide CPF / CNPJ",
-      //     (value: any) => cpf.isValid(value) || cnpj.isValid(value)),  
-       
-      //   billingAdress: Yup.string().required(),
-      //   billingNumber: Yup.string().required(),
-      //   billingNeightborhood: Yup.string().required(),
-      //   billingState: Yup.string().required(),
-      //   billingCity: Yup.string().required(),
-      // billingZipCode: Yup.string().required(), 
-      // creditCardNumber: Yup.string()
-      //   .when("paymentType", (paymentType, schema) => {
-      //   return paymentType === "credit_card" ?   schema.required() : schema
-      //   }),
-      
-      //  creditCardExpiration: Yup.string()
-      //   .when("paymentType", (paymentType, schema) => {
-      //   return paymentType === "credit_card" ?   schema.required() : schema
-      //   }),
-      //   creditCardHolderName: Yup.string()
-      //   .when("paymentType", (paymentType, schema) => {
-      //   return paymentType === "credit_card" ?   schema.required() : schema
-      //   }),
-      //    creditCardCvv: Yup.string()
-      //   .when("paymentType", (paymentType, schema) => {
-      //   return paymentType === "credit_card" ?   schema.required() : schema
-      // })
-    })       
-    
+      //     (value: any) => cpf.isValid(value) || cnpj.isValid(value)),
+
+      billingAdress: Yup.string().required(),
+      billingNumber: Yup.string().required(),
+      billingNeightborhood: Yup.string().required(),
+      billingState: Yup.string().required(),
+      billingCity: Yup.string().required(),
+      billingZipCode: Yup.string().required(),
+      creditCardNumber: Yup.string().when('paymentType', (paymentType, schema) => {
+        return paymentType === 'credit_card' ? schema.required() : schema
+      }),
+
+      creditCardExpiration: Yup.string().when('paymentType', (paymentType, schema) => {
+        return paymentType === 'credit_card' ? schema.required() : schema
+      }),
+      creditCardHolderName: Yup.string().when('paymentType', (paymentType, schema) => {
+        return paymentType === 'credit_card' ? schema.required() : schema
+      }),
+      creditCardCvv: Yup.string().when('paymentType', (paymentType, schema) => {
+        return paymentType === 'credit_card' ? schema.required() : schema
+      }),
+    })
+
     if (!(await schema.isValid(req.body))) {
-      return res.json({msg:  "Erro on Validate Schema"})
-    }
-    else {
-      return res.json({msg: "Schema Success"})
-      
+      return res.json({ msg: 'Erro on Validate Schema' })
+    } else {
+      return res.json({ msg: 'Schema Success' })
     }
 
     // await prisma.transactions.create({
@@ -108,7 +103,6 @@ async function register(req: Request, res: Response) {
     //     billingZipCode: req.body.billingZipCode,
     //   },
     // })
-  
 
     // return res.status(200).json({ msg: 'Success!!' })
   } catch (error) {
